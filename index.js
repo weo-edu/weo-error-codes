69be2f77d75694324acfa3b6d3ccbb3387584a7a
Andrew Shaffer <andrew@weo.io>
2014-01-23 17:17:28 -0800

    add back in express res intercept capability

--- index.js
+++ index.js
@@ -5,33 +5,43 @@ var defaultCodes = ["missing", "missing_field", "invalid", "already_exists"];
 
 module.exports = function(codes) {
   codes = defaultCodes.concat(codes);
-  return function(message) {
+  return function(req, res, next) {
     function ClientError(message) {
       this.message = message;
       this.errors = [];
     }
 
+    ClientError.prototype.send = function(status) {
+      res.json(status, this);
+    };
+
     ClientError.prototype.fromSails = function(resource, sailsError) {
       var self = this;
-      if(sailsError.ValidationError) {
-        this.message = 'ValidationError';
-        _.each(sailsError.ValidationError, function(errors, field) {
-          _.each(errors, function(err) {
-            var code;
-            switch(err.rule) {
-            case 'unique':
-              code = 'already_exists';
-              break;
-            case 'required':
-              code = 'missing_field';
-              break;
-            default:
-              code = 'invalid';
-              break;
-            }
-            self[code](resource, field, err);
+      _.each([].concat(sailsError), fromSails);
+      return this;
+
+      function fromSails(sailsError) {
+        if(sailsError.ValidationError) {
+          this.message = 'ValidationError';
+          _.each(sailsError.ValidationError, function(errors, field) {
+            _.each(errors, function(err) {
+              var code;
+              switch(err.rule) {
+              case 'unique':
+                code = 'already_exists';
+                break;
+              case 'required':
+                code = 'missing_field';
+                break;
+              default:
+                code = 'invalid';
+                break;
+              }
+              delete err.message;
+              self[code](resource, field, err);
+            });
           });
-        });
+        }
       }
     };
 
@@ -47,6 +57,9 @@ module.exports = function(codes) {
       };
     });
 
-    return new ClientError(message);
+    res.clientError = function(message) {
+      return (new ClientError(message));
+    };
+    next();
   };
 };
